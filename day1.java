5a65110ea166ce5313dbcd8061955d3fb7f878d1
Krishna0408 <98581299+Krishna0408@users.noreply.github.com>
2023-02-23 14:26:49 +0000

    Day 1

--- day1.java
+++ day1.java
@@ -6,7 +6,7 @@ public class day1{
         int a[][]= {{1000,2000,3000},{4000},{6000,5000},{9000,7000,8000},{10000}};
         int b[]= new int[a.length];
         int i,j;
-        int sum=0;
+        int sum=0; 
         for(i=0;i<a.length;i++)
         {
             for(j=0;j<a[i].length;j++)
