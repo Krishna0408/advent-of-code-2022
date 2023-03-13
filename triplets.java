f981470eaac3cd3eb5af20a259be4047f89a448b
Krishna0408 <98581299+Krishna0408@users.noreply.github.com>
2023-03-13 15:11:30 +0530

    Java codes

--- triplets.java
+++ triplets.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class triplets {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int n = sc.nextInt(); 
         int a[] =new int[n];
         for (int i=0;i<n;i++) {
             a[i]=sc.nextInt();
