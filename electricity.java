a3b4cc0e9c09080fb69ff21f29951ef6dccfdf9d
Krishna0408 <98581299+Krishna0408@users.noreply.github.com>
2023-03-13 15:10:28 +0530

    Java codes

--- electricity.java
+++ electricity.java
@@ -28,7 +28,7 @@ if(id == 1){
 for(int i=0;i<1;i++){
 for(int j=0;j<4;j++){
     int c=Integer.parseInt(a[0][3]); 
-    if(j==3){
+    if(j==3){ 
         if(c<101){
             System.out.println(c);
             }
